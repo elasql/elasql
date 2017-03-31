bd24548f816e04fb0d9a030708938b24277383f0
slmt <sam123456777@gmail.com>
2017-03-31 17:34:43 +0800

    Update the design of group comm. interface for clients.

---
+++ src/main/java/org/elasql/remote/groupcomm/client/BatchSpcSender.java
@@ -0,0 +1,94 @@
+package org.elasql.remote.groupcomm.client;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.elasql.remote.groupcomm.StoredProcedureCall;
+import org.elasql.util.ElasqlProperties;
+import org.vanilladb.comm.client.ClientAppl;
+
+class BatchSpcSender implements Runnable {
+	private static Logger logger = Logger.getLogger(BatchSpcSender.class.getName());
+
+	private final static int BATCH_SIZE;
+	private final static long MAX_WAITING_TIME; // in ms
+
+	static {
+		BATCH_SIZE = ElasqlProperties.getLoader()
+				.getPropertyAsInteger(GroupCommConnection.class.getName() + ".BATCH_SIZE", 1);
+		MAX_WAITING_TIME = ElasqlProperties.getLoader()
+				.getPropertyAsInteger(GroupCommConnection.class.getName() + ".MAX_WAITING_TIME", 1000);
+	}
+
+	private AtomicInteger numOfQueuedSpcs = new AtomicInteger(0);
+	private Queue<StoredProcedureCall> spcQueue = new ConcurrentLinkedQueue<StoredProcedureCall>();
+	private ClientAppl clientAppl;
+	private long lastSendingTime;
+	private int nodeId;
+
+	public BatchSpcSender(int id, ClientAppl appl) {
+		clientAppl = appl;
+		lastSendingTime = System.currentTimeMillis();
+		nodeId = id;
+	}
+
+	@Override
+	public void run() {
+		// periodically send batch of requests
+		if (logger.isLoggable(Level.INFO))
+			logger.info("start batching-request worker thread");
+
+		while (true)
+			sendBatchRequestToDb();
+	}
+
+	public void callStoredProc(int rteId, int pid, Object... pars) {
+		StoredProcedureCall spc = new StoredProcedureCall(nodeId, rteId, pid, pars);
+		spcQueue.add(spc);
+		int size = numOfQueuedSpcs.incrementAndGet();
+		
+		if (size >= BATCH_SIZE) {
+			synchronized (this) {
+				notifyAll();
+			}
+		}
+	}
+
+	private void sendBatchRequestToDb() {
+		// Waiting for the queue reaching the threshold
+		int size = numOfQueuedSpcs.get();
+		long currentTime = System.currentTimeMillis();
+		try {
+			while (size < BATCH_SIZE && (currentTime - lastSendingTime < MAX_WAITING_TIME || size < 1)) {
+				
+				synchronized (this) {
+					wait(100);
+				}
+				
+				size = numOfQueuedSpcs.get();
+				currentTime = System.currentTimeMillis();
+			}
+			
+			lastSendingTime = currentTime;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// Send a batch of requests
+		StoredProcedureCall[] batchSpc = new StoredProcedureCall[size];
+		StoredProcedureCall spc;
+		for (int i = 0; i < batchSpc.length; i++) {
+			spc = spcQueue.poll();
+			
+			if (spc == null)
+				throw new RuntimeException("Something wrong");
+			
+			batchSpc[i] = spc;
+		}
+		numOfQueuedSpcs.addAndGet(-size);
+		clientAppl.sendRequest(batchSpc);
+	}
+}
---
+++ src/main/java/org/elasql/remote/groupcomm/client/GroupCommConnection.java
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ * Copyright 2016 vanilladb.org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.elasql.remote.groupcomm.client;
+
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import org.elasql.remote.groupcomm.ClientResponse;
+import org.vanilladb.comm.client.ClientAppl;
+import org.vanilladb.comm.client.ClientNodeFailListener;
+import org.vanilladb.comm.client.ClientP2pMessageListener;
+import org.vanilladb.comm.messages.ChannelType;
+import org.vanilladb.comm.messages.P2pMessage;
+import org.vanilladb.core.remote.storedprocedure.SpResultSet;
+
+public class GroupCommConnection implements ClientP2pMessageListener, ClientNodeFailListener {
+
+	// RTE id -> A blocking queue of responses from servers
+	private Map<Integer, BlockingQueue<ClientResponse>> rteToRespQueue = new ConcurrentHashMap<Integer, BlockingQueue<ClientResponse>>();
+	// RTE id -> The transaction number of the received response last time
+	private Map<Integer, Long> rteToLastTxNum = new ConcurrentHashMap<Integer, Long>();
+
+	private BatchSpcSender batchSender;
+	private int myId;
+
+	public GroupCommConnection(int id) {
+		myId = id;
+
+		// Initialize group communication
+		ClientAppl clientAppl = new ClientAppl(id, this, this);
+		clientAppl.start();
+		// wait for all servers to start up
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		clientAppl.startPFD();
+
+		// Start the batch sender
+		batchSender = new BatchSpcSender(id, clientAppl);
+		new Thread(null, batchSender, "Batch-Spc-Sender").start();
+	}
+
+	public SpResultSet callStoredProc(int rteId, int pid, Object... pars) {
+		// Check if there is a queue for it
+		BlockingQueue<ClientResponse> respQueue = rteToRespQueue.get(rteId);
+		if (respQueue == null) {
+			respQueue = new LinkedBlockingQueue<ClientResponse>();
+			rteToRespQueue.put(rteId, respQueue);
+		}
+
+		batchSender.callStoredProc(rteId, pid, pars);
+
+		// Wait for the response
+		try {
+			ClientResponse cr = respQueue.take();
+			long lastTxNum = rteToLastTxNum.get(rteId);
+
+			while (lastTxNum >= cr.getTxNum())
+				cr = respQueue.take();
+
+			// Record the tx number of the response
+			rteToLastTxNum.put(rteId, cr.getTxNum());
+			
+			return (SpResultSet) cr.getResultSet();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Something wrong");
+		}
+	}
+
+	@Override
+	public void onRecvClientP2pMessage(P2pMessage p2pmsg) {
+		ClientResponse c = (ClientResponse) p2pmsg.getMessage();
+		
+		// Check if this response is for this node
+		if (c.getClientId() == myId) {
+			rteToRespQueue.get(c.getRteId()).add(c);
+		} else {
+			throw new RuntimeException("Something wrong");
+		}
+	}
+
+	@Override
+	public void onNodeFail(int id, ChannelType channelType) {
+		// do nothing
+	}
+}
