edf79b8765ff1c5dee87966b2d46f2a3ee7f94f3
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2017-07-04 12:52:06 +0800

    Remove some code

--- src/main/java/org/elasql/cache/calvin/CalvinCacheMgr.java
+++ src/main/java/org/elasql/cache/calvin/CalvinCacheMgr.java
@@ -85,7 +85,6 @@ public class CalvinCacheMgr {
 		rec = VanillaCoreCrud.read(key, tx);
 		if (rec != null) {
 			rec.setSrcTxNum(tx.getTransactionNumber());
-			rec.setLocal(true);
 			cachedRecords.put(key, rec);
 		}
 
@@ -94,7 +93,7 @@ public class CalvinCacheMgr {
 
 	public CachedRecord readFromRemote(RecordKey key) {
 		CachedRecord rec = cachedRecords.get(key);
-		if (rec != null )
+		if (rec != null)
 			return rec;
 
 		if (inbox == null)
@@ -115,7 +114,6 @@ public class CalvinCacheMgr {
 
 		return rec;
 	}
-
 
 	public void update(RecordKey key, CachedRecord rec) {
 		rec.setSrcTxNum(tx.getTransactionNumber());
