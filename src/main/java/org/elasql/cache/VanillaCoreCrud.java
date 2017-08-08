0453e36afcc748b7f94ca535f0cccbaa5efb95d7
slmt <sam123456777@gmail.com>
2017-08-09 01:09:04 +0800

    Update for multi-key-indexes

--- src/main/java/org/elasql/cache/VanillaCoreCrud.java
+++ src/main/java/org/elasql/cache/VanillaCoreCrud.java
@@ -17,10 +17,12 @@ package org.elasql.cache;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Set;
 
-import org.elasql.server.Elasql;
 import org.elasql.sql.RecordKey;
 import org.vanilladb.core.query.algebra.Plan;
 import org.vanilladb.core.query.algebra.SelectPlan;
@@ -28,10 +30,11 @@ import org.vanilladb.core.query.algebra.SelectScan;
 import org.vanilladb.core.query.algebra.TablePlan;
 import org.vanilladb.core.query.algebra.UpdateScan;
 import org.vanilladb.core.query.algebra.index.IndexSelectPlan;
+import org.vanilladb.core.server.VanillaDb;
 import org.vanilladb.core.sql.Constant;
 import org.vanilladb.core.sql.ConstantRange;
-import org.vanilladb.core.sql.Schema;
 import org.vanilladb.core.storage.index.Index;
+import org.vanilladb.core.storage.index.SearchKey;
 import org.vanilladb.core.storage.metadata.index.IndexInfo;
 import org.vanilladb.core.storage.record.RecordId;
 import org.vanilladb.core.storage.tx.Transaction;
@@ -42,29 +45,24 @@ import org.vanilladb.core.storage.tx.Transaction;
 public class VanillaCoreCrud {
 
 	public static CachedRecord read(RecordKey key, Transaction tx) {
-		// Open index select scan
-		TablePlan tp = new TablePlan(key.getTableName(), tx);
-		Schema sch = tp.schema();
-		Map<String, IndexInfo> indexInfoMap = Elasql.catalogMgr()
-				.getIndexInfo(key.getTableName(), tx);
-		Plan p = tp;
-		for (String fld : key.getKeyFldSet()) {
-			IndexInfo ii = indexInfoMap.get(fld);
-			if (ii != null) {
-				p = new IndexSelectPlan(tp, ii, ConstantRange.newInstance(key
-						.getKeyVal(fld)), tx);
-				break;
-			}
-		}
-
-		p = new SelectPlan(p, key.getPredicate());
-		SelectScan s = (SelectScan) p.open();
+		String tblName = key.getTableName();
+		TablePlan tp = new TablePlan(tblName, tx);
+		Plan selectPlan = null;
+		
+		// Create a IndexSelectPlan if there is matching index in the predicate
+		selectPlan = selectByBestMatchedIndex(tblName, tp, key, tx);
+		if (selectPlan == null)
+			selectPlan = new SelectPlan(tp, key.getPredicate());
+		else
+			selectPlan = new SelectPlan(selectPlan, key.getPredicate());
+		
+		SelectScan s = (SelectScan) selectPlan.open();
 		s.beforeFirst();
 		CachedRecord rec = null;
 
 		if (s.next()) {
 			Map<String, Constant> fldVals = new HashMap<String, Constant>();
-			for (String fld : sch.fields())
+			for (String fld : tp.schema().fields())
 				fldVals.put(fld, s.getVal(fld));
 			rec = new CachedRecord(fldVals);
 		}
@@ -74,140 +72,257 @@ public class VanillaCoreCrud {
 	}
 
 	public static void update(RecordKey key, CachedRecord rec, Transaction tx) {
-		TablePlan tp = new TablePlan(key.getTableName(), tx);
-		Map<String, IndexInfo> indexInfoMap = Elasql.catalogMgr()
-				.getIndexInfo(key.getTableName(), tx);
-
-		// get target fields
-		Collection<String> targetflds = rec.getDirtyFldNames();
-
-		// XXX: Remove search key from target fields if they existed
-		// for Constraint C1 below
-		for (String searchFld : key.getKeyFldSet())
-			targetflds.remove(searchFld);
-
-		// open all indexes associate with target fields
-		HashMap<String, Index> targetIdxMap = new HashMap<String, Index>();
-		for (String fld : targetflds) {
-			IndexInfo ii = indexInfoMap.get(fld);
-			Index idx = (ii == null) ? null : ii.open(tx);
-			if (idx != null)
-				targetIdxMap.put(fld, idx);
-		}
-
-		// create a IndexSelectPlan
+		String tblName = key.getTableName();
+		TablePlan tp = new TablePlan(tblName, tx);
 		Plan selectPlan = null;
-		for (String fld : indexInfoMap.keySet()) {
-			ConstantRange cr = key.getPredicate().constantRange(fld);
-			if (cr != null && !targetflds.contains(fld)) { // Constraint C1
-				IndexInfo ii = indexInfoMap.get(fld);
-				selectPlan = new IndexSelectPlan(tp, ii, cr, tx);
-				break;
-			}
-		}
+		
+		// Create a IndexSelectPlan if there is matching index in the predicate
+		selectPlan = selectByBestMatchedIndex(tblName, tp, key, tx, rec.getDirtyFldNames());
 		if (selectPlan == null)
 			selectPlan = new SelectPlan(tp, key.getPredicate());
 		else
 			selectPlan = new SelectPlan(selectPlan, key.getPredicate());
+		
+		// Open all indexes associate with target fields
+		Set<Index> modifiedIndexes = new HashSet<Index>();
+		for (String fieldName : rec.getDirtyFldNames()) {
+			List<IndexInfo> iiList = VanillaDb.catalogMgr().getIndexInfo(tblName, fieldName, tx);
+			for (IndexInfo ii : iiList)
+				modifiedIndexes.add(ii.open(tx));
+		}
+		
+		// Open the scan
 		UpdateScan s = (UpdateScan) selectPlan.open();
 		s.beforeFirst();
-
-		// the record key should identifies one record uniquely
-		if (s.next()) {
-			Constant newval, oldval;
-			for (String fld : targetflds) {
-				newval = rec.getVal(fld);
-				oldval = s.getVal(fld);
-				if (newval.equals(oldval))
-					continue;
-				// update the appropriate index, if it exists
-				Index idx = targetIdxMap.get(fld);
-				if (idx != null) {
-					RecordId rid = s.getRecordId();
-					idx.delete(oldval, rid, true);
-					idx.insert(newval, rid, true);
-				}
-				s.setVal(fld, newval);
+		int count = 0;
+		while (s.next()) {
+			
+			// Construct a mapping from field names to values
+			Map<String, Constant> oldValMap = new HashMap<String, Constant>();
+			Map<String, Constant> newValMap = new HashMap<String, Constant>();
+			for (String fieldName : rec.getDirtyFldNames()) {
+				Constant oldVal = s.getVal(fieldName);
+				Constant newVal = rec.getVal(fieldName);
+				
+				oldValMap.put(fieldName, oldVal);
+				newValMap.put(fieldName, newVal);
+				s.setVal(fieldName, newVal);
 			}
+			
+			RecordId rid = s.getRecordId();
+			
+			// Update the indexes
+			for (Index index : modifiedIndexes) {
+				// Construct a SearchKey for the old value
+				Map<String, Constant> fldValMap = new HashMap<String, Constant>();
+				for (String fldName : index.getIndexInfo().fieldNames()) {
+					Constant oldVal = oldValMap.get(fldName);
+					if (oldVal == null)
+						oldVal = s.getVal(fldName);
+					fldValMap.put(fldName, oldVal);
+				}
+				SearchKey oldKey = new SearchKey(index.getIndexInfo().fieldNames(), fldValMap);
+				
+				// Delete the old value from the index
+				index.delete(oldKey, rid, true);
+				
+				// Construct a SearchKey for the new value
+				fldValMap = new HashMap<String, Constant>();
+				for (String fldName : index.getIndexInfo().fieldNames()) {
+					Constant newVal = newValMap.get(fldName);
+					if (newVal == null)
+						newVal = s.getVal(fldName);
+					fldValMap.put(fldName, newVal);
+				}
+				SearchKey newKey = new SearchKey(index.getIndexInfo().fieldNames(), fldValMap);
+				
+				// Insert the new value to the index
+				index.insert(newKey, rid, true);
+				
+				index.close();
+			}
+			
+			count++;
 		}
-		// close opened indexes
-		for (String fld : targetflds) {
-			Index idx = targetIdxMap.get(fld);
-			if (idx != null)
-				idx.close();
-		}
+		
+		// Close opened indexes and the record file
+		for (Index index : modifiedIndexes)
+			index.close();
 		s.close();
 
 		// XXX: Do we need this ?
-		// VanillaDdDb.statMgr().countRecordUpdates(tblname, 1);
+		// VanillaDdDb.statMgr().countRecordUpdates(tblname, count);
 	}
 
 	public static void insert(RecordKey key, CachedRecord rec, Transaction tx) {
 		String tblname = key.getTableName();
 		Plan p = new TablePlan(tblname, tx);
-		Map<String, IndexInfo> indexes = Elasql.catalogMgr().getIndexInfo(
-				tblname, tx);
-		Map<String, Constant> m = rec.getFldValMap();
 
-		// first, insert the record
+		// Insert the record into the record file
 		UpdateScan s = (UpdateScan) p.open();
 		s.insert();
-		RecordId rid = s.getRecordId();
-
-		// then modify each field, inserting an index record if appropriate
-		for (Entry<String, Constant> e : m.entrySet()) {
-			Constant val = e.getValue();
-			if (val == null)
-				continue;
-			// first, insert into index
-			IndexInfo ii = indexes.get(e.getKey());
-			if (ii != null) {
-				Index idx = ii.open(tx);
-				idx.insert(val, rid, true);
-				idx.close();
-			}
-			// insert into record file
-			s.setVal(e.getKey(), val);
+		for (Map.Entry<String, Constant> fldValPair : rec.getFldValMap().entrySet()) {
+			s.setVal(fldValPair.getKey(), fldValPair.getValue());
 		}
+		RecordId rid = s.getRecordId();
 		s.close();
-
+		
+		// Insert the record to all corresponding indexes
+		Set<IndexInfo> indexes = new HashSet<IndexInfo>();
+		for (String fldname : rec.getFldNames()) {
+			List<IndexInfo> iis = VanillaDb.catalogMgr().getIndexInfo(tblname, fldname, tx);
+			indexes.addAll(iis);
+		}
+		
+		for (IndexInfo ii : indexes) {
+			Index idx = ii.open(tx);
+			idx.insert(new SearchKey(ii.fieldNames(), rec.getFldValMap()), rid, true);
+			idx.close();
+		}
+		
 		// XXX: Do we need this ?
 		// VanillaDdDb.statMgr().countRecordUpdates(tblname, 1);
 	}
 
 	public static void delete(RecordKey key, Transaction tx) {
-		String tblname = key.getTableName();
-		TablePlan tp = new TablePlan(tblname, tx);
-		Map<String, IndexInfo> indexInfoMap = Elasql.catalogMgr()
-				.getIndexInfo(tblname, tx);
-
-		Plan p = tp;
-		for (String fld : key.getKeyFldSet()) {
-			IndexInfo ii = indexInfoMap.get(fld);
-			if (ii != null) {
-				p = new IndexSelectPlan(tp, ii, ConstantRange.newInstance(key
-						.getKeyVal(fld)), tx);
-				break;
-			}
+		String tblName = key.getTableName();
+		TablePlan tp = new TablePlan(tblName, tx);
+		Plan selectPlan = null;
+		
+		// Create a IndexSelectPlan if there is matching index in the predicate
+		boolean usingIndex = false;
+		selectPlan = selectByBestMatchedIndex(tblName, tp, key, tx);
+		if (selectPlan == null)
+			selectPlan = new SelectPlan(tp, key.getPredicate());
+		else {
+			selectPlan = new SelectPlan(selectPlan, key.getPredicate());
+			usingIndex = true;
 		}
-		p = new SelectPlan(p, key.getPredicate());
-		UpdateScan s = (UpdateScan) p.open();
+		
+		// Retrieve all indexes
+		List<IndexInfo> allIndexes = new LinkedList<IndexInfo>();
+		Set<String> indexedFlds = VanillaDb.catalogMgr().getIndexedFields(tblName, tx);
+		
+		for (String indexedFld : indexedFlds) {
+			List<IndexInfo> iis = VanillaDb.catalogMgr().getIndexInfo(tblName, indexedFld, tx);
+			allIndexes.addAll(iis);
+		}
+		
+		// Open the scan
+		UpdateScan s = (UpdateScan) selectPlan.open();
+		int count = 0;
 		s.beforeFirst();
-		// the record key should identifies one record uniquely
-		if (s.next()) {
+		while (s.next()) {
 			RecordId rid = s.getRecordId();
-			// delete the record from every index
-			for (String fldname : indexInfoMap.keySet()) {
-				Constant val = s.getVal(fldname);
-				Index idx = indexInfoMap.get(fldname).open(tx);
-				idx.delete(val, rid, true);
-				idx.close();
+			
+			// Delete the record from every index
+			for (IndexInfo ii : allIndexes) {
+				// Construct a key-value map
+				Map<String, Constant> fldValMap = new HashMap<String, Constant>();
+				for (String fldName : ii.fieldNames())
+					fldValMap.put(fldName, s.getVal(fldName));
+				SearchKey searchKey = new SearchKey(ii.fieldNames(), fldValMap);
+				
+				// Delete from the index
+				Index index = ii.open(tx);
+				index.delete(searchKey, rid, true);
+				index.close();
 			}
+			
+			// Delete the record from the record file
 			s.delete();
+
+			/*
+			 * Re-open the index select scan to ensure the correctness of
+			 * next(). E.g., index block before delete the current slot ^:
+			 * [^5,5,6]. After the deletion: [^5,6]. When calling next() of
+			 * index select scan, current slot pointer will move forward,
+			 * [5,^6].
+			 */
+			if (usingIndex) {
+				s.close();
+				s = (UpdateScan) selectPlan.open();
+				s.beforeFirst();
+			}
+			
+			count++;
 		}
 		s.close();
 
 		// XXX: Do we need this ?
-		// VanillaDdDb.statMgr().countRecordUpdates(tblname, 1);
+		// VanillaDdDb.statMgr().countRecordUpdates(tblname, count);
+	}
+	
+	private static IndexSelectPlan selectByBestMatchedIndex(String tblName,
+			TablePlan tablePlan, RecordKey key, Transaction tx) {
+
+		// Look up candidate indexes
+		Set<IndexInfo> candidates = new HashSet<IndexInfo>();
+		for (String fieldName : key.getKeyFldSet()) {
+			List<IndexInfo> iis = VanillaDb.catalogMgr().getIndexInfo(tblName, fieldName, tx);
+			candidates.addAll(iis);
+		}
+		
+		return selectByBestMatchedIndex(candidates, tablePlan, key, tx);
+	}
+	
+	private static IndexSelectPlan selectByBestMatchedIndex(String tblName,
+			TablePlan tablePlan, RecordKey key, Transaction tx, Collection<String> excludedFields) {
+		
+		Set<IndexInfo> candidates = new HashSet<IndexInfo>();
+		for (String fieldName : key.getKeyFldSet()) {
+			if (excludedFields.contains(fieldName))
+				continue;
+			
+			List<IndexInfo> iis = VanillaDb.catalogMgr().getIndexInfo(tblName, fieldName, tx);
+			for (IndexInfo ii : iis) {
+				boolean ignored = false;
+				for (String fldName : ii.fieldNames())
+					if (excludedFields.contains(fldName)) {
+						ignored = true;
+						break;
+					}
+				
+				if (!ignored)
+					candidates.add(ii);
+			}
+		}
+		
+		return selectByBestMatchedIndex(candidates, tablePlan, key, tx);
+	}
+	
+	private static IndexSelectPlan selectByBestMatchedIndex(Set<IndexInfo> candidates,
+			TablePlan tablePlan, RecordKey key, Transaction tx) {
+		
+		// Choose the index with the most matched fields in the predicate
+		int matchedCount = 0;
+		IndexInfo bestIndex = null;
+		Map<String, ConstantRange> searchRanges = null;
+		
+		for (IndexInfo ii : candidates) {
+			if (ii.fieldNames().size() < matchedCount)
+				continue;
+			
+			Map<String, ConstantRange> ranges = new HashMap<String, ConstantRange>();
+			for (String fieldName : ii.fieldNames()) {
+				Constant val = key.getKeyVal(fieldName);
+				if (val == null)
+					continue;
+				
+				ranges.put(fieldName, ConstantRange.newInstance(val));
+			}
+			
+			if (ranges.size() > matchedCount) {
+				matchedCount = ranges.size();
+				bestIndex = ii;
+				searchRanges = ranges;
+			}
+		}
+		
+		if (bestIndex != null) {
+			return new IndexSelectPlan(tablePlan, bestIndex, searchRanges, tx);
+		}
+		
+		return null;
 	}
 }
