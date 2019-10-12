59ddbf913e6be818d04f3726e04e10e5601434bc
SLMT <sam123456777@gmail.com>
2019-10-12 15:34:39 +0800

    Quick fix: do not migrate insertion-only tables for TPC-C

--- src/main/java/org/elasql/schedule/tpart/graph/LapTGraph.java
+++ src/main/java/org/elasql/schedule/tpart/graph/LapTGraph.java
@@ -42,11 +42,18 @@ public class LapTGraph extends TGraph {
 			}
 		}
 		
-		// Put the rest of the records on where they are
+		// Handle the rest of written records
 		for (Entry<RecordKey, TxNode> resPosPair : resPos.entrySet()) {
 			RecordKey res = resPosPair.getKey();
 			TxNode node = resPosPair.getValue();
-			node.addWriteBackEdges(new Edge(sinkNodes[node.getPartId()], res));
+			
+			// Quick fix: ignore insert-only tables
+			if (res.getTableName().equals("orders") || res.getTableName().equals("new_order") ||
+					res.getTableName().equals("order_line"))
+				node.addWriteBackEdges(new Edge(sinkNodes[partMgr.getPartition(res)], res));
+			else
+			// Put the records on where they are
+				node.addWriteBackEdges(new Edge(sinkNodes[node.getPartId()], res));
 		}
 		
 		// Clear the resource map for the next run
