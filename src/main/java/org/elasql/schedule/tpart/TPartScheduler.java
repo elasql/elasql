110fc2dd30a7e55dd68c0b6a658d22080e55f66c
FrankCCCCC <unaxultraspaceos5@gmail.com>
2021-08-17 17:23:04 +0800

    reset TPartSchedular and fix intention

--- src/main/java/org/elasql/schedule/tpart/TPartScheduler.java
+++ src/main/java/org/elasql/schedule/tpart/TPartScheduler.java
@@ -26,7 +26,6 @@ import org.elasql.storage.tx.recovery.DdRecoveryMgr;
 import org.elasql.util.ElasqlProperties;
 import org.vanilladb.core.server.VanillaDb;
 import org.vanilladb.core.server.task.Task;
-import org.vanilladb.comm.server.VanillaCommServer;
 
 public class TPartScheduler extends Task implements Scheduler {
 	private static Logger logger = Logger.getLogger(TPartScheduler.class.getName());
@@ -34,8 +33,8 @@ public class TPartScheduler extends Task implements Scheduler {
 	private static final int SCHEDULE_BATCH_SIZE;
 
 	private TPartStoredProcedureFactory factory;
-
-	// private File dumpDir = new File("batch_dump");
+	
+//	private File dumpDir = new File("batch_dump");
 
 	static {
 		SCHEDULE_BATCH_SIZE = ElasqlProperties.getLoader()
@@ -50,12 +49,13 @@ public class TPartScheduler extends Task implements Scheduler {
 	private long startTime, sinkStartTime, sinkStopTime, threadInitStartTime;
 	private boolean isFirst = true;
 
-	public TPartScheduler(TPartStoredProcedureFactory factory, BatchNodeInserter inserter, Sinker sinker,
-			TGraph graph) {
+	public TPartScheduler(TPartStoredProcedureFactory factory, 
+			BatchNodeInserter inserter, Sinker sinker, TGraph graph) {
 		this(factory, inserter, sinker, graph, true);
 	}
-
-	public TPartScheduler(TPartStoredProcedureFactory factory, BatchNodeInserter inserter, Sinker sinker, TGraph graph,
+	
+	public TPartScheduler(TPartStoredProcedureFactory factory, 
+			BatchNodeInserter inserter, Sinker sinker, TGraph graph,
 			boolean isBatching) {
 		this.factory = factory;
 		this.inserter = inserter;
@@ -63,13 +63,13 @@ public class TPartScheduler extends Task implements Scheduler {
 		this.graph = graph;
 		this.batchingEnabled = isBatching;
 		this.spcQueue = new LinkedBlockingQueue<StoredProcedureCall>();
-
+		
 		// Clear the dump dir
-		// dumpDir.mkdirs();
-		// for (File file : dumpDir.listFiles()) {
-		// if (file.isFile())
-		// file.delete();
-		// }
+//		dumpDir.mkdirs();
+//		for (File file : dumpDir.listFiles()) {
+//			if (file.isFile())
+//				file.delete();
+//		}
 	}
 
 	public void schedule(StoredProcedureCall call) {
@@ -82,7 +82,7 @@ public class TPartScheduler extends Task implements Scheduler {
 
 	public void run() {
 		List<TPartStoredProcedureTask> batchedTasks = new LinkedList<TPartStoredProcedureTask>();
-
+		
 		while (true) {
 			try {
 				// blocked if the queue is empty
@@ -92,23 +92,24 @@ public class TPartScheduler extends Task implements Scheduler {
 				// schedules the utility procedures directly without T-Part
 				// module
 				if (task.getProcedureType() == ProcedureType.UTILITY) {
-					// VanillaDb.taskMgr().runTask(task);
+//					VanillaDb.taskMgr().runTask(task);
 					continue;
 				}
 
 				// TODO: Uncomment this when the migration module is migrated
-				// if (task.getProcedureType() == ProcedureType.MIGRATION) {
-				// // Process and dispatch it immediately
-				// processMigrationTx(task);
-				// continue;
-				// }
+//				if (task.getProcedureType() == ProcedureType.MIGRATION) {
+//					// Process and dispatch it immediately
+//					processMigrationTx(task);
+//					continue;
+//				}
 
 				if (task.getProcedureType() == ProcedureType.NORMAL) {
 					batchedTasks.add(task);
 				}
-
+				
 				// sink current t-graph if # pending tx exceeds threshold
-				if ((batchingEnabled && batchedTasks.size() >= SCHEDULE_BATCH_SIZE) || !batchingEnabled) {
+				if ((batchingEnabled && batchedTasks.size() >= SCHEDULE_BATCH_SIZE)
+						|| !batchingEnabled) {
 					processBatch(batchedTasks);
 					batchedTasks.clear();
 				}
@@ -119,7 +120,7 @@ public class TPartScheduler extends Task implements Scheduler {
 			}
 		}
 	}
-
+	
 	private void processBatch(List<TPartStoredProcedureTask> batchedTasks) {
 
 		startTime = System.nanoTime();
@@ -130,17 +131,14 @@ public class TPartScheduler extends Task implements Scheduler {
 			
 		// Insert the batch of tasks
 		inserter.insertBatch(graph, batchedTasks);
-
-		// HERE
-		// batchedTasks.
-
+		
 		// Debug
-		// printGraphStatistics();
-		// System.out.println(graph);
-		// printStatistics();
-		// printImbalStatistics();
-		// collectGraphStatistics();
-
+//		printGraphStatistics();
+//		System.out.println(graph);
+//		printStatistics();
+//		printImbalStatistics();
+//		collectGraphStatistics();
+		
 		// Sink the graph
 		if (graph.getTxNodes().size() != 0) {
 			sinkStartTime = System.nanoTime();
@@ -151,17 +149,17 @@ public class TPartScheduler extends Task implements Scheduler {
 			dispatchToTaskMgr(plansTter);
 		}
 	}
-
+	
 	// TODO: Uncomment this when the migration module is migrated
-	// private void processMigrationTx(TPartStoredProcedureTask task) {
-	// // Insert the task to T-Graph
-	// ColdMigrationProcedure sp = (ColdMigrationProcedure) task.getProcedure();
-	// graph.insertTxNode(task, sp.getMigrationRange().getDestPartId());
-	//
-	// // Sink the graph
-	// Iterator<TPartStoredProcedureTask> plansTter = sinker.sink(graph);
-	// dispatchToTaskMgr(plansTter);
-	// }
+//	private void processMigrationTx(TPartStoredProcedureTask task) {
+//		// Insert the task to T-Graph
+//		ColdMigrationProcedure sp = (ColdMigrationProcedure) task.getProcedure();
+//		graph.insertTxNode(task, sp.getMigrationRange().getDestPartId());
+//		
+//		// Sink the graph
+//		Iterator<TPartStoredProcedureTask> plansTter = sinker.sink(graph);
+//		dispatchToTaskMgr(plansTter);
+//	}
 
 	private TPartStoredProcedureTask createStoredProcedureTask(StoredProcedureCall call) {
 		if (call.isNoOpStoredProcCall()) {
@@ -185,63 +183,61 @@ public class TPartScheduler extends Task implements Scheduler {
 			VanillaDb.taskMgr().runTask(p);
 		}
 	}
-
+	
 	private int batchId = 0;
 	private int imbalanced = 0;
 	private int remoteTxRead = 0;
 	private int remoteSinkRead = 0;
 	private int recordCount = 0;
 	private long nextReportTime = 0;
-
+	
 	private void printGraphStatistics() {
 		// XXX: Show the statistics of the T-Graph
 		long time = (System.currentTimeMillis() - Elasql.START_TIME_MS) / 1000;
-		// if (batchId % 100 == 0) {
-		// String stat = graph.getStatistics();
-		// System.out.println("Time: " + time);
-		// System.out.println("T-Graph id: " + (batchId + 1));
-		// System.out.print(stat);
-
-		imbalanced += graph.getImbalancedDis();
-		remoteTxRead += graph.getRemoteTxReads();
-		remoteSinkRead += graph.getRemoteSinkReads();
-		recordCount++;
-
-		if (time >= nextReportTime) {
-			// System.out.println("======== Total Statistics ========");
-			System.out.println(
-					String.format("Time: %d, avg. imbal: %f, avg. remote tx reads: %f, " + "avg. remote sink reads: %f",
-							time, ((double) imbalanced) / recordCount, ((double) remoteTxRead) / recordCount,
-							((double) remoteSinkRead) / recordCount));
-			// System.out.println("==================================\n");
-
-			imbalanced = 0;
-			remoteTxRead = 0;
-			remoteSinkRead = 0;
-			recordCount = 0;
-			nextReportTime = time + 5;
-
-			// Dump the current graph
-			// GraphDumper.dumpToFile(new File(dumpDir, String.format("%d_%d.txt", time,
-			// batchId)), graph);
-		}
-		// }
+//		if (batchId % 100 == 0) {
+//			String stat = graph.getStatistics();
+//			System.out.println("Time: " + time);
+//			System.out.println("T-Graph id: " + (batchId + 1));
+//			System.out.print(stat);
+			
+			imbalanced += graph.getImbalancedDis();
+			remoteTxRead += graph.getRemoteTxReads();
+			remoteSinkRead += graph.getRemoteSinkReads();
+			recordCount++;
+			
+			if (time >= nextReportTime) {
+//				System.out.println("======== Total Statistics ========");
+				System.out.println(String.format("Time: %d, avg. imbal: %f, avg. remote tx reads: %f, "
+						+ "avg. remote sink reads: %f", time, ((double) imbalanced) / recordCount,
+						((double) remoteTxRead) / recordCount, ((double) remoteSinkRead) / recordCount));
+//				System.out.println("==================================\n");
+				
+				imbalanced = 0;
+				remoteTxRead = 0;
+				remoteSinkRead = 0;
+				recordCount = 0;
+				nextReportTime = time + 5;
+				
+				// Dump the current graph
+//				GraphDumper.dumpToFile(new File(dumpDir, String.format("%d_%d.txt", time, batchId)), graph);
+			}
+//		}
 		batchId++;
 	}
-
+	
 	private int[] numberOfRemoteReads = new int[PartitionMetaMgr.NUM_PARTITIONS];
 	private int[] numberOfTxns = new int[PartitionMetaMgr.NUM_PARTITIONS];
 	private int[] numberOfDistTxns = new int[PartitionMetaMgr.NUM_PARTITIONS];
 	private double[] ratioOfDistTxns = new double[PartitionMetaMgr.NUM_PARTITIONS];
-
+	
 	private void collectGraphStatistics() {
 		long time = (System.currentTimeMillis() - Elasql.START_TIME_MS) / 1000;
-
+		
 		// collects
 		for (TxNode node : graph.getTxNodes()) {
 			int masterId = node.getPartId();
 			boolean distTx = false;
-
+			
 			// count remote reads
 			for (Edge e : node.getReadEdges()) {
 				int resPartId = e.getTarget().getPartId();
@@ -250,18 +246,18 @@ public class TPartScheduler extends Task implements Scheduler {
 					distTx = true;
 				}
 			}
-
+			
 			// count transactions
 			numberOfTxns[masterId]++;
 			if (distTx) {
 				numberOfDistTxns[masterId]++;
 			}
 		}
-
+		
 		// prints every period of time
 		if (time >= nextReportTime) {
 			System.out.println(String.format("Time: %d", time));
-
+			
 			// Count the % of dist. transactions
 			for (int i = 0; i < ratioOfDistTxns.length; i++) {
 				if (numberOfTxns[i] == 0) {
@@ -271,40 +267,45 @@ public class TPartScheduler extends Task implements Scheduler {
 					ratioOfDistTxns[i] /= numberOfTxns[i];
 				}
 			}
-
+			
 			// # of transactions
-			System.out.println(String.format("Number of transactions: %s.", Arrays.toString(numberOfTxns)));
+			System.out.println(String.format("Number of transactions: %s.",
+					Arrays.toString(numberOfTxns)));
 			Arrays.fill(numberOfTxns, 0);
-
+			
 			// # of dist transactions
-			System.out.println(String.format("Number of dist. transactions: %s.", Arrays.toString(numberOfDistTxns)));
+			System.out.println(String.format("Number of dist. transactions: %s.",
+					Arrays.toString(numberOfDistTxns)));
 			Arrays.fill(numberOfDistTxns, 0);
-
+			
 			// % of dist transactions
-			System.out.println(String.format("Ratio of dist. transactions: %s.", Arrays.toString(ratioOfDistTxns)));
-
+			System.out.println(String.format("Ratio of dist. transactions: %s.",
+					Arrays.toString(ratioOfDistTxns)));
+			
 			// # of remote reads
-			System.out.println(String.format("Number of remote reads: %s.", Arrays.toString(numberOfRemoteReads)));
+			System.out.println(String.format("Number of remote reads: %s.",
+					Arrays.toString(numberOfRemoteReads)));
 			Arrays.fill(numberOfRemoteReads, 0);
-
+			
 			nextReportTime = time + 15;
 		}
 	}
-
+	
 	private int totalImbalanced = 0;
 	private int numOfBatches = 0;
-
+	
 	private void printImbalStatistics() {
 		long time = (System.currentTimeMillis() - Elasql.START_TIME_MS) / 1000;
-
+		
 		numOfBatches++;
 		totalImbalanced += graph.getImbalancedDis();
-
+		
 		if (time >= nextReportTime) {
 			double averageImb = totalImbalanced;
 			averageImb /= numOfBatches;
-			System.out.println(String.format("Average imbalanced: %f at time %d.", averageImb, time));
-
+			System.out.println(String.format("Average imbalanced: %f at time %d.",
+					averageImb, time));
+			
 			numOfBatches = 0;
 			totalImbalanced = 0;
 			nextReportTime = time + 3;
