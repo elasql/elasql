0991164f879172cc4b9a18f10cb85bab1cc4957d
SLMT <sam123456777@gmail.com>
2021-10-05 23:05:43 +0800

    Add a method to get an average of CPU loads over time

--- src/main/java/org/elasql/perf/tpart/metric/TpartMetricWarehouse.java
+++ src/main/java/org/elasql/perf/tpart/metric/TpartMetricWarehouse.java
@@ -1,31 +1,52 @@
 package org.elasql.perf.tpart.metric;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import org.elasql.perf.MetricWarehouse;
+import org.elasql.storage.metadata.PartitionMetaMgr;
 import org.vanilladb.core.server.task.Task;
 
 public class TpartMetricWarehouse extends Task implements MetricWarehouse {
-
+	
+	private static class StampedMetric<M> {
+		long timestamp;
+		M metric;
+		
+		StampedMetric(long timestamp, M metric) {
+			this.timestamp = timestamp;
+			this.metric = metric;
+		}
+	}
+	
 	private BlockingQueue<TPartSystemMetrics> metricQueue;
 	
-	private Map<Integer, Double> systemCpuLoad;
-	private Map<Integer, Double> processCpuLoad;
-	private Map<Integer, Double> systemLoadAverage;
+	// Load metrics
+	private Map<Integer, List<StampedMetric<Double>>> systemCpuLoad;
+	private Map<Integer, List<StampedMetric<Double>>> processCpuLoad;
+	private Map<Integer, List<StampedMetric<Double>>> systemLoadAverage;
 	
-	private Map<Integer, Integer> threadActiveCount;
+	// Thread metrics
+	private Map<Integer, List<StampedMetric<Integer>>> threadActiveCount;
 	
 	public TpartMetricWarehouse() {
-		this.metricQueue = new LinkedBlockingQueue<TPartSystemMetrics>();
+		metricQueue = new LinkedBlockingQueue<TPartSystemMetrics>();
 		
-		this.processCpuLoad = new HashMap<Integer, Double>();
-		this.systemCpuLoad = new HashMap<Integer, Double>();
-		this.systemLoadAverage = new HashMap<Integer, Double>();
+		processCpuLoad = new HashMap<Integer, List<StampedMetric<Double>>>();
+		systemCpuLoad = new HashMap<Integer, List<StampedMetric<Double>>>();
+		systemLoadAverage = new HashMap<Integer, List<StampedMetric<Double>>>();
+		threadActiveCount = new HashMap<Integer, List<StampedMetric<Integer>>>();
 		
-		this.threadActiveCount = new HashMap<Integer, Integer>();
+		for (int nodeId = 0; nodeId < PartitionMetaMgr.NUM_PARTITIONS; nodeId++) {
+			processCpuLoad.put(nodeId, new ArrayList<StampedMetric<Double>>());
+			systemCpuLoad.put(nodeId, new ArrayList<StampedMetric<Double>>());
+			systemLoadAverage.put(nodeId, new ArrayList<StampedMetric<Double>>());
+			threadActiveCount.put(nodeId, new ArrayList<StampedMetric<Integer>>());
+		}
 	}
 	
 	public void receiveMetricReport(TPartSystemMetrics metrics) {
@@ -47,46 +68,72 @@ public class TpartMetricWarehouse extends Task implements MetricWarehouse {
 	}
 	
 	private synchronized void recordMetric(TPartSystemMetrics metrics) {
-		processCpuLoad.put(metrics.getServerId(), metrics.getProcessCpuLoad());
-		systemCpuLoad.put(metrics.getServerId(), metrics.getSystemCpuLoad());
-		systemLoadAverage.put(metrics.getServerId(), metrics.getSystemLoadAverage());
+		long timestamp = System.currentTimeMillis();
 		
-		threadActiveCount.put(metrics.getServerId(),  metrics.getThreadActiveCount());
+		processCpuLoad.get(metrics.getServerId()).add(
+				new StampedMetric<>(timestamp, metrics.getProcessCpuLoad()));
+		systemCpuLoad.get(metrics.getServerId()).add(
+				new StampedMetric<>(timestamp, metrics.getSystemCpuLoad()));
+		systemLoadAverage.get(metrics.getServerId()).add(
+				new StampedMetric<>(timestamp, metrics.getSystemLoadAverage()));
+		threadActiveCount.get(metrics.getServerId()).add(
+				new StampedMetric<>(timestamp, metrics.getThreadActiveCount()));
 		
 		// debug code
-		// System.out.println(String.format("Receives a report from server %d with average system load: %f",
-		//  metrics.getServerId(), metrics.getSystemLoadAverage()));
+//		System.out.println(String.format("Receives a metric report from server %d with CPU load: %f",
+//				metrics.getServerId(), metrics.getSystemCpuLoad()));
 	}
 	
 	public synchronized double getProcessCpuLoad(int serverId) {
-		Double load = processCpuLoad.get(serverId);
-		if (load == null)
+		List<StampedMetric<Double>> history = processCpuLoad.get(serverId);
+		if (history.isEmpty()) {
 			return 0.0;
-		else
-			return load;
+		} else {
+			return history.get(history.size() - 1).metric;
+		}
 	}
 	
 	public synchronized double getSystemCpuLoad(int serverId) {
-		Double load = systemCpuLoad.get(serverId);
-		if (load == null)
+		List<StampedMetric<Double>> history = systemCpuLoad.get(serverId);
+		if (history.isEmpty()) {
 			return 0.0;
-		else
-			return load;
+		} else {
+			return history.get(history.size() - 1).metric;
+		}
+	}
+	
+	public synchronized double getAveragedSystemCpuLoad(int serverId, long timeLength) {
+		long startTime = System.currentTimeMillis() - timeLength;
+		double sum = 0.0;
+		double count = 0.0;
+
+		List<StampedMetric<Double>> history = systemCpuLoad.get(serverId);
+		for (int idx = history.size() - 1; idx >= 0; idx--) {
+			StampedMetric<Double> metric = history.get(idx);
+			if (metric.timestamp < startTime)
+				break;
+			sum += metric.metric;
+			count += 1.0;
+		}
+		
+		return sum / count;
 	}
 	
 	public synchronized double getSystemLoadAverage(int serverId) {
-		Double load = systemLoadAverage.get(serverId);
-		if (load == null)
+		List<StampedMetric<Double>> history = systemLoadAverage.get(serverId);
+		if (history.isEmpty()) {
 			return 0.0;
-		else
-			return load;
+		} else {
+			return history.get(history.size() - 1).metric;
+		}
 	}
 	
 	public synchronized int getThreadActiveCount(int serverId) {
-		Integer count = threadActiveCount.get(serverId);
-		if (count == null)
+		List<StampedMetric<Integer>> history = threadActiveCount.get(serverId);
+		if (history.isEmpty()) {
 			return 0;
-		else
-			return count;
+		} else {
+			return history.get(history.size() - 1).metric;
+		}
 	}
 }
