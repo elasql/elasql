ba94017f90722943ad33ce80f818828c3f028dad
Yuxuan <xuan19970825@gmail.com>
2022-06-23 10:56:52 +0800

    Add offline RL model

---
+++ src/main/java/org/elasql/perf/tpart/rl/model/Imitation.java
@@ -0,0 +1,107 @@
+package org.elasql.perf.tpart.rl.model;
+
+import java.util.Random;
+
+import org.elasql.perf.tpart.rl.util.Memory;
+import org.elasql.perf.tpart.rl.util.MemoryBatch;
+
+import ai.djl.Model;
+import ai.djl.engine.Engine;
+import ai.djl.inference.Predictor;
+import ai.djl.ndarray.NDArray;
+import ai.djl.ndarray.NDList;
+import ai.djl.ndarray.NDManager;
+import ai.djl.nn.Parameter;
+import ai.djl.training.GradientCollector;
+import ai.djl.training.loss.SoftmaxCrossEntropyLoss;
+import ai.djl.training.optimizer.Optimizer;
+import ai.djl.training.tracker.Tracker;
+import ai.djl.translate.NoopTranslator;
+import ai.djl.translate.TranslateException;
+import ai.djl.util.Pair;
+
+public class Imitation {
+	protected static final float MIN_EXPLORE_RATE = 0.1f;
+	protected static final float DECAY_EXPLORE_RATE = 0.99f;
+
+	protected final Random random = new Random(0);
+	protected Memory memory = new Memory(80);
+	protected Model imitation_net;
+
+	protected final int dim_of_state_space;
+	protected final int num_of_actions;
+	protected final int hidden_size;
+	protected final float learning_rate;
+
+	protected final int batch_size;
+
+	private final SoftmaxCrossEntropyLoss loss_func = new SoftmaxCrossEntropyLoss();
+	private Optimizer optimizer;
+
+	protected NDManager manager;
+	protected Predictor<NDList, NDList> imitation_predictor;
+
+	protected Imitation(int dim_of_state_space, int num_of_actions, int hidden_size, int batch_size,
+			float learning_rate) {
+		this.dim_of_state_space = dim_of_state_space;
+		this.num_of_actions = num_of_actions;
+		this.hidden_size = hidden_size;
+		this.batch_size = batch_size;
+		this.learning_rate = learning_rate;
+		
+		reset();
+
+	}
+
+	protected Imitation(int dim_of_state_space, int num_of_actions, int hidden_size, int batch_size,
+			float learning_rate, Memory memory) {
+		this.dim_of_state_space = dim_of_state_space;
+		this.num_of_actions = num_of_actions;
+		this.hidden_size = hidden_size;
+		this.batch_size = batch_size;
+		this.learning_rate = learning_rate;
+		this.memory = memory;
+
+		reset();
+	}
+
+	protected final void reset() {
+		optimizer = Optimizer.adam().optLearningRateTracker(Tracker.fixed(learning_rate)).build();
+
+		if (manager != null) {
+			manager.close();
+		}
+		manager = NDManager.newBaseManager();
+		imitation_net = ImitationModel.newModel(manager, dim_of_state_space, hidden_size, num_of_actions);
+
+		imitation_predictor = imitation_net.newPredictor(new NoopTranslator());
+	}
+
+	protected final void gradientUpdate(NDArray i_loss) {
+		try (GradientCollector collector = Engine.getInstance().newGradientCollector()) {
+			collector.backward(i_loss);
+			for (Pair<String, Parameter> params : imitation_net.getBlock().getParameters()) {
+				NDArray params_arr = params.getValue().getArray();
+				optimizer.update(params.getKey(), params_arr, params_arr.getGradient());
+			}
+		}
+	}
+
+	public void updateModel(NDManager manager) throws TranslateException {
+		MemoryBatch batch = memory.sampleBatch(batch_size, manager);
+		NDArray action = batch.getActions();
+
+		// imitation part
+		NDArray imitation = imitation_predictor.predict(new NDList(batch.getStates())).singletonOrThrow();
+        
+		NDArray i_loss = loss_func.evaluate(new NDList(action), new NDList(imitation));
+        i_loss.setRequiresGradient(true);
+//        System.out.println(i_loss.toString());
+		gradientUpdate(i_loss);
+
+	}
+	
+	public Model getImitationNet() {
+		return imitation_net;
+	}
+}
---
+++ src/main/java/org/elasql/perf/tpart/rl/model/OfflineBCQ.java
@@ -0,0 +1,50 @@
+package org.elasql.perf.tpart.rl.model;
+
+import org.elasql.perf.tpart.rl.util.ActionSampler;
+import org.elasql.perf.tpart.rl.util.Helper;
+import org.elasql.perf.tpart.rl.util.Memory;
+import org.elasql.perf.tpart.rl.util.MemoryBatch;
+
+import ai.djl.ndarray.NDArray;
+import ai.djl.ndarray.NDList;
+import ai.djl.ndarray.NDManager;
+import ai.djl.training.loss.L2Loss;
+import ai.djl.translate.TranslateException;
+
+public class OfflineBCQ extends OfflineBaseBCQ {
+    private final L2Loss loss_func = new L2Loss();
+
+    public OfflineBCQ(int dim_of_state_space, int num_of_actions, int hidden_size, int batch_size, int sync_net_interval,
+            float gamma, float learning_rate, Memory memory) {
+        super(dim_of_state_space, num_of_actions, hidden_size, batch_size, sync_net_interval, gamma, learning_rate, memory);
+    }
+
+    @Override
+    protected int getAction(NDManager manager, float[] state) throws TranslateException {    	
+    	NDArray imitation = imitation_predictor.predict(new NDList(manager.create(state))).singletonOrThrow();
+    	imitation = imitation.softmax(-1);
+    	NDArray score = target_predictor.predict(new NDList(manager.create(state))).singletonOrThrow();
+        return ActionSampler.epsilonGreedy(score, imitation, random, 0);
+    }
+
+    @Override
+	public void updateModel(NDManager manager) throws TranslateException {
+        MemoryBatch batch = memory.sampleBatch(batch_size, manager);
+        
+        NDArray policy = policy_predictor.predict(new NDList(batch.getStates())).singletonOrThrow();
+        NDArray target = target_predictor.predict(new NDList(batch.getNextStates())).singletonOrThrow().duplicate();
+        NDArray expected_returns = Helper.gather(policy, batch.getActions().toIntArray());
+        // Reward from our current policy
+        NDArray next_returns = batch.getRewards()
+                .add(target.max(new int[] { 1 }).mul(batch.getMasks().logicalNot()).mul(gamma));        
+        
+        //q_loss = F.smooth_l1_loss(current_Q, target_Q)
+		//i_loss = F.nll_loss(imt, action.reshape(-1))
+		//Q_loss = q_loss + i_loss + 1e-2 * i.pow(2).mean()
+        NDArray q_loss = loss_func.evaluate(new NDList(expected_returns), new NDList(next_returns));
+//        System.out.println(q_loss.toString());
+        gradientUpdate(q_loss);
+
+    }
+
+}
